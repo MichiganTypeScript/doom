2f48bafa8c26922015e8dddd8b07ed2ea2504300
Dimitri Mitropoulos <dimitrimitropoulos@gmail.com>
2024-03-07 17:54:57 -0500

    wip: getting there?

--- packages/ts-type-math/divide-playground.ts
+++ packages/ts-type-math/divide-playground.ts
@@ -1,36 +1,329 @@
 import { Expect, Extends } from "type-testing"
-import { _DivideBinary32 } from "./divide"
-import { _ } from "vitest/dist/reporters-rzC174PQ.js"
+import { _DivideBinaryArbitrary } from "./divide"
 
-type divisor4 =  '0011' // M
-type fallback4 = '0000' // A
-type dividend4 = '0111' // Q
 
-type s0 = _DivideBinary32<dividend4, divisor4, fallback4, 0, true>
-//   ^?
-type t0 = Expect<Extends<s0, { M: '0011'; A: '0000', Q: '0111' }>>
+// 4 digits
+// 16 union members
+type divisor_4 =   '0011' // M
+type fallback_4 =  '0000' // A
+type dividend_4 =  '0111' // Q
+type quotient_4 =  '0010' // result
+type remainder_4 = '0001' // result
+type stopAt_4 = 3
 
-type s1 = _DivideBinary32<dividend4, divisor4, fallback4, 1, true>
-//   ^?
-type t1 = Expect<Extends<s1, { M: '0011'; A: '0000', Q: '1110' }>>
-
-type s2 = _DivideBinary32<dividend4, divisor4, fallback4, 2, true>
-// //   ^?
-type t2 = Expect<Extends<s2, { M: '0011'; A: '0001', Q: '1100' }>>
-
-type s3 = _DivideBinary32<dividend4, divisor4, fallback4, 3, true>
-//   ^?
-type t3 = Expect<Extends<s3, { M: '0011'; A: '0000', Q: '1001' }>>
-
-type s4 = _DivideBinary32<dividend4, divisor4, fallback4, 4, true>
-//   ^?
-type t4 = Expect<Extends<s4, { M: '0011'; A: '0001', Q: '0010' }>>
-
-type DivideBinary4<
+type DivideBinary_4<
   dividend extends string,
   divisor extends string
-> = _DivideBinary32<dividend, divisor, '0000', 3>
+> = _DivideBinaryArbitrary<dividend, divisor, fallback_4, stopAt_4>
 
-type ss4 = DivideBinary4<dividend4, divisor4>
+
+
+
+
+type x_4 = DivideBinary_4<dividend_4, divisor_4>
 //   ^?
-type tt4 = Expect<Extends<ss4, { quotient: '0010', remainder: '0001' }>>
+type tx_4 = Expect<Extends<x_4, { quotient: quotient_4, remainder: remainder_4 }>>
+
+type s0_4 = _DivideBinaryArbitrary<dividend_4, divisor_4, fallback_4, 0, true>
+//   ^?
+type t0_4 = Expect<Extends<s0_4, { A: fallback_4, Q: dividend_4 }>>
+
+type s1_4 = _DivideBinaryArbitrary<dividend_4, divisor_4, fallback_4, 1, true>
+//   ^?
+type t1_4 = Expect<Extends<s1_4, { A: '0000', Q: '1110' }>>
+
+type s2_4 = _DivideBinaryArbitrary<dividend_4, divisor_4, fallback_4, 2, true>
+//   ^?
+type t2_4 = Expect<Extends<s2_4, { A: '0001', Q: '1100' }>>
+
+type s3_4 = _DivideBinaryArbitrary<dividend_4, divisor_4, fallback_4, 3, true>
+//   ^?
+type t3_4 = Expect<Extends<s3_4, { A: '0000', Q: '1001' }>>
+
+type s_4 = _DivideBinaryArbitrary<dividend_4, divisor_4, fallback_4, 4, true>
+//   ^?
+type t_4 = Expect<Extends<s_4, { A: remainder_4, Q: quotient_4 }>>
+
+
+// 5 digits
+// 32 union members
+type divisor_5 =   '00011' // M
+type fallback_5 =  '00000' // A
+type dividend_5 =  '00111' // Q
+type quotient_5 =  '00010' // result
+type remainder_5 = '00001' // result
+type stopAt_5 = 4
+
+type DivideBinary_5<
+  dividend extends string,
+  divisor extends string
+> = _DivideBinaryArbitrary<dividend, divisor, fallback_5, stopAt_5>
+
+type x_5 = DivideBinary_5<dividend_5, divisor_5>
+//   ^?
+type tx_5 = Expect<Extends<x_5, { quotient: quotient_5, remainder: remainder_5 }>>
+
+type s0_5 = _DivideBinaryArbitrary<dividend_5, divisor_5, fallback_5, 0, true>
+//   ^?
+type t0_5 = Expect<Extends<s0_5, { A: fallback_5, Q: dividend_5 }>>
+
+type s1_5 = _DivideBinaryArbitrary<dividend_5, divisor_5, fallback_5, 1, true>
+//   ^?
+type t1_5 = Expect<Extends<s1_5, { A: '00000', Q: '01110' }>>
+
+type s2_5 = _DivideBinaryArbitrary<dividend_5, divisor_5, fallback_5, 2, true>
+//   ^?
+type t2_5 = Expect<Extends<s2_5, { A: '00000', Q: '11100' }>>
+
+type s3_5 = _DivideBinaryArbitrary<dividend_5, divisor_5, fallback_5, 3, true>
+//   ^?
+type t3_5 = Expect<Extends<s3_5, { A: '00001', Q: '11000' }>>
+
+type s4_5 = _DivideBinaryArbitrary<dividend_5, divisor_5, fallback_5, 4, true>
+//   ^?
+type t4_5 = Expect<Extends<s4_5, { A: '00000', Q: '10001' }>>
+
+type s_5 = _DivideBinaryArbitrary<dividend_5, divisor_5, fallback_5, 5, true>
+//   ^?
+type t_5 = Expect<Extends<s_5, { A: remainder_5, Q: quotient_5 }>>
+
+
+
+// 6 digits
+// 64 union members
+type divisor_6 =   '000011' // M
+type fallback_6 =  '000000' // A
+type dividend_6 =  '000111' // Q
+type quotient_6 =  '000010' // result
+type remainder_6 = '000001' // result
+type stopAt_6 = 5
+
+type DivideBinary_6<
+  dividend extends string,
+  divisor extends string
+> = _DivideBinaryArbitrary<dividend, divisor, fallback_6, stopAt_6>
+
+type x_6 = DivideBinary_6<dividend_6, divisor_6>
+//   ^?
+type tx_6 = Expect<Extends<x_6, { quotient: quotient_6, remainder: remainder_6 }>>
+
+type s0_6 = _DivideBinaryArbitrary<dividend_6, divisor_6, fallback_6, 0, true>
+//   ^?
+type t0_6 = Expect<Extends<s0_6, { A: fallback_6, Q: dividend_6 }>>
+
+type s1_6 = _DivideBinaryArbitrary<dividend_6, divisor_6, fallback_6, 1, true>
+//   ^?
+type t1_6 = Expect<Extends<s1_6, { A: '000000', Q: '001110' }>>
+
+type s2_6 = _DivideBinaryArbitrary<dividend_6, divisor_6, fallback_6, 2, true>
+//   ^?
+type t2_6 = Expect<Extends<s2_6, { A: '000000', Q: '011100' }>>
+
+type s3_6 = _DivideBinaryArbitrary<dividend_6, divisor_6, fallback_6, 3, true>
+//   ^?
+type t3_6 = Expect<Extends<s3_6, { A: '000000', Q: '111000' }>>
+
+type s4_6 = _DivideBinaryArbitrary<dividend_6, divisor_6, fallback_6, 4, true>
+//   ^?
+type t4_6 = Expect<Extends<s4_6, { A: '000001', Q: '110000' }>>
+
+type s5_6 = _DivideBinaryArbitrary<dividend_6, divisor_6, fallback_6, 5, true>
+//   ^?
+type t5_6 = Expect<Extends<s5_6, { A: '000000', Q: '100001' }>>
+
+type s_6 = _DivideBinaryArbitrary<dividend_6, divisor_6, fallback_6, 6, true>
+//   ^?
+type t_6 = Expect<Extends<s_6, { A: remainder_6, Q: quotient_6 }>>
+
+
+
+
+// 7 digits
+// 128 union members
+type divisor_7 =   '0000011' // M
+type fallback_7 =  '0000000' // A
+type dividend_7 =  '0000111' // Q
+type quotient_7 =  '0000010' // result
+type remainder_7 = '0000001' // result
+type stopAt_7 = 6
+
+type DivideBinary_7<
+  dividend extends string,
+  divisor extends string
+> = _DivideBinaryArbitrary<dividend, divisor, fallback_7, stopAt_7>
+
+type x_7 = DivideBinary_7<dividend_7, divisor_7>
+//   ^?
+type tx_7 = Expect<Extends<x_7, { quotient: quotient_7, remainder: remainder_7 }>>
+
+type s0_7 = _DivideBinaryArbitrary<dividend_7, divisor_7, fallback_7, 0, true>
+//   ^?
+type t0_7 = Expect<Extends<s0_7, { A: fallback_7, Q: dividend_7 }>>
+
+type s1_7 = _DivideBinaryArbitrary<dividend_7, divisor_7, fallback_7, 1, true>
+//   ^?
+type t1_7 = Expect<Extends<s1_7, { A: '0000000', Q: '0001110' }>>
+
+type s2_7 = _DivideBinaryArbitrary<dividend_7, divisor_7, fallback_7, 2, true>
+//   ^?
+type t2_7 = Expect<Extends<s2_7, { A: '0000000', Q: '0011100' }>>
+
+type s3_7 = _DivideBinaryArbitrary<dividend_7, divisor_7, fallback_7, 3, true>
+//   ^?
+type t3_7 = Expect<Extends<s3_7, { A: '0000000', Q: '0111000' }>>
+
+type s4_7 = _DivideBinaryArbitrary<dividend_7, divisor_7, fallback_7, 4, true>
+//   ^?
+type t4_7 = Expect<Extends<s4_7, { A: '0000000', Q: '1110000' }>>
+
+type s5_7 = _DivideBinaryArbitrary<dividend_7, divisor_7, fallback_7, 5, true>
+//   ^?
+type t5_7 = Expect<Extends<s5_7, { A: '0000001', Q: '1100000' }>>
+
+type s6_7 = _DivideBinaryArbitrary<dividend_7, divisor_7, fallback_7, 6, true>
+//   ^?
+type t6_7 = Expect<Extends<s6_7, { A: '0000000', Q: '1000001' }>>
+
+type s_7 = _DivideBinaryArbitrary<dividend_7, divisor_7, fallback_7, 7, true>
+//   ^?
+type t_7 = Expect<Extends<s_7, { A: remainder_7, Q: quotient_7 }>>
+
+
+// 8 digits
+// 256 union members
+type divisor_8 =   '00000011' // M
+type fallback_8 =  '00000000' // A
+type dividend_8 =  '00000111' // Q
+type quotient_8 =  '00000010' // result
+type remainder_8 = '00000001' // result
+type stopAt_8 = 7
+
+type DivideBinary_8<
+  dividend extends string,
+  divisor extends string
+> = _DivideBinaryArbitrary<dividend, divisor, fallback_8, stopAt_8>
+
+type x_8 = DivideBinary_8<dividend_8, divisor_8>
+//   ^?
+type tx_8 = Expect<Extends<x_8, { quotient: quotient_8, remainder: remainder_8 }>>
+
+type s0_8 = _DivideBinaryArbitrary<dividend_8, divisor_8, fallback_8, 0, true>
+//   ^?
+type t0_8 = Expect<Extends<s0_8, { A: fallback_8, Q: dividend_8 }>>
+
+type s1_8 = _DivideBinaryArbitrary<dividend_8, divisor_8, fallback_8, 1, true>
+//   ^?
+type t1_8 = Expect<Extends<s1_8, { A: '00000000', Q: '00001110' }>>
+
+type s2_8 = _DivideBinaryArbitrary<dividend_8, divisor_8, fallback_8, 2, true>
+//   ^?
+type t2_8 = Expect<Extends<s2_8, { A: '00000000', Q: '00011100' }>>
+
+type s3_8 = _DivideBinaryArbitrary<dividend_8, divisor_8, fallback_8, 3, true>
+//   ^?
+type t3_8 = Expect<Extends<s3_8, { A: '00000000', Q: '00111000' }>>
+
+type s4_8 = _DivideBinaryArbitrary<dividend_8, divisor_8, fallback_8, 4, true>
+//   ^?
+type t4_8 = Expect<Extends<s4_8, { A: '00000000', Q: '01110000' }>>
+
+type s5_8 = _DivideBinaryArbitrary<dividend_8, divisor_8, fallback_8, 5, true>
+//   ^?
+type t5_8 = Expect<Extends<s5_8, { A: '00000000', Q: '11100000' }>>
+
+type s6_8 = _DivideBinaryArbitrary<dividend_8, divisor_8, fallback_8, 6, true>
+//   ^?
+type t6_8 = Expect<Extends<s6_8, { A: '00000001', Q: '11000000' }>>
+
+type s7_8 = _DivideBinaryArbitrary<dividend_8, divisor_8, fallback_8, 7, true>
+//   ^?
+type t7_8 = Expect<Extends<s7_8, { A: '00000000', Q: '10000001' }>>
+
+type s_8 = _DivideBinaryArbitrary<dividend_8, divisor_8, fallback_8, 8, true>
+//   ^?
+type t_8 = Expect<Extends<s_8, { A: remainder_8, Q: quotient_8 }>>
+
+// 9 digits
+// 512 union members
+
+type divisor_9 =   '000000011' // M
+type fallback_9 =  '000000000' // A
+type dividend_9 =  '000000111' // Q
+type quotient_9 =  '000000010' // result
+type remainder_9 = '000000001' // result
+type stopAt_9 = 8
+
+type DivideBinary_9<
+  dividend extends string,
+  divisor extends string
+> = _DivideBinaryArbitrary<dividend, divisor, fallback_9, stopAt_9>
+
+type x_9 = DivideBinary_9<dividend_9, divisor_9>
+//   ^?
+type tx_9 = Expect<Extends<x_9, { quotient: quotient_9, remainder: remainder_9 }>>
+
+
+// 10 digits
+// 1024 union members
+
+type divisor_10 =   '0000000011' // M
+type fallback_10 =  '0000000000' // A
+type dividend_10 =  '0000000111' // Q
+type quotient_10 =  '0000000010' // result
+type remainder_10 = '0000000001' // result
+type stopAt_10 = 9
+
+type DivideBinary_10<
+  dividend extends string,
+  divisor extends string
+> = _DivideBinaryArbitrary<dividend, divisor, fallback_10, stopAt_10>
+
+
+
+
+
+
+
+type x_10 = DivideBinary_10<dividend_10, divisor_10>
+//   ^?
+type tx_10 = Expect<Extends<x_10, { quotient: quotient_10, remainder: remainder_10 }>>
+
+
+// 11 digits
+// 2048 union members
+
+type divisor_11 =   '00000000011' // M
+type fallback_11 =  '00000000000' // A
+type dividend_11 =  '00000000111' // Q
+type quotient_11 =  '00000000010' // result
+type remainder_11 = '00000000001' // result
+type stopAt_11 = 10
+
+type DivideBinary_11<
+  dividend extends string,
+  divisor extends string
+> = _DivideBinaryArbitrary<dividend, divisor, fallback_11, stopAt_11>
+
+type x_11 = DivideBinary_11<dividend_11, divisor_11>
+//   ^?
+type tx_11 = Expect<Extends<x_11, { quotient: quotient_11, remainder: remainder_11 }>>
+
+
+// 12 digits
+// 4096 union members
+
+type divisor_12 =   '000000000011' // M
+type fallback_12 =  '000000000000' // A
+type dividend_12 =  '000000000111' // Q
+type quotient_12 =  '000000000010' // result
+type remainder_12 = '000000000001' // result
+type stopAt_12 = 11
+
+type DivideBinary_12<
+  dividend extends string,
+  divisor extends string
+> = _DivideBinaryArbitrary<dividend, divisor, fallback_12, stopAt_12>
+
+type x_12 = DivideBinary_12<dividend_12, divisor_12>
+//   ^?
+type tx_12 = Expect<Extends<x_12, { quotient: quotient_12, remainder: remainder_12 }>>
--- packages/ts-type-math/divide.ts
+++ packages/ts-type-math/divide.ts
@@ -33,12 +33,11 @@ type ShiftLeftOnceBinaryArbitraryFill0<
   : 'you dun goofed0'
 
 
-export type _DivideBinary32<
+export type _DivideBinaryArbitrary<
   Q extends string, // = dividend
   M extends string, // = divisor
   A extends string,
   StopAt extends number,
-  debugMode extends boolean = false,
 
   Count extends 1[] = [],
 
@@ -71,8 +70,7 @@ export type _DivideBinary32<
     : ShiftLeftOnceBinaryArbitraryFill1<Q>,
 
 > =
-  Count['length'] extends StopAt
-  ? debugMode extends false
+  `${Count['length']}` extends `${StopAt}`
     ? {
         quotient: _newQ
         remainder:
@@ -81,26 +79,13 @@ export type _DivideBinary32<
           : _newA
       }
 
-    : {
-        M: M,
-        A: A,
-        Q: Q,
-        _AShift: _AShift,
-        _AShiftMinusM: _AShiftMinusM,
-        _newA: _newA,
-        _newQ: _newQ,
-        quotient: _newQ,
-
-      }
-
-  : _DivideBinary32<
-      _newQ,
-      M,
+    : _DivideBinaryArbitrary<
+        _newQ,
+        M,
       _newA,
-      StopAt,
-      debugMode,
-      [1, ...Count]
-    >
+        StopAt,
+        [1, ...Count]
+      >
 
 export type DivideUnsignedBinary32<
   dividend extends string,
@@ -110,120 +95,9 @@ export type DivideUnsignedBinary32<
   dividend extends divisor ? Wasm.I32True : // if equal return 1
   divisor extends Wasm.I32True ? dividend : // if divide by 1 return dividend
 
-  _DivideBinary32<
+  _DivideBinaryArbitrary<
     dividend,
     divisor,
     Wasm.I32False,
     31
-  >
-
-// import { ReverseString8Segments, SignBit } from "./binary";
-// import { SubtractBinary } from "./subtract";
-// import { WasmValue, Wasm } from "./wasm";
-// import { Clamp } from './split'
-
-// /*
-//   Yes, I need this reference. I'm not good at this stuff.  Truly.
-
-//   Dividend / Divisor = Quotient
-
-//            Quotient
-//           __________
-//   Divisor | Dividend
-
-//   Dividend
-//   --------  =  Quotient
-//   Divisor
-// */
-
-// // what follows is an attempt to implement the restoring division algorithm via https://www.youtube.com/watch?v=Q8GPrxlbQy4
-
-// type Combine<a extends string, b extends string> = `${a}${b}`;
-
-// type zeroes = '00000000000000000000000000000000'
-
-// type ShiftLeftOnceBinaryArbitrary<
-//   a extends string,
-
-//   /** should be '0' or '1' */
-//   fillWith extends string,
-// > =
-//   a extends `${infer lolGtfohYouSillyBit}${infer tail}` // get it? silly bit........ch? it's 2am. gimmie a break.
-//   ? `${tail}${fillWith}`
-//   : never
-
-
-// type ShiftRightOnceBinaryArbitrary<
-//   a extends string,
-//   fillWith extends '0' | '1',
-// > = ReverseString8Segments<
-//   Clamp.First32Bits<`${fillWith}${a}`>
-// >
-
-// type StopAt = 32 // maybe this needs to be 32?
-// type divisor ="00000000000000000000000000000011"
-// type dividend="00000000000000000000000000011010"
-// // export type e = _DivideBinary32<dividend, divisor>  // =>
-// // type d = DivideBinary32<dividend, divisor>   // =>
-// // type r = RemainderBinary32<dividend, divisor>// =>
-
-// type _DivideBinary32<
-//   dividend extends WasmValue, // dividend
-//   divisor extends WasmValue,  // divisor
-
-//   /** dividend remainder */
-//   dr extends WasmValue = Combine<Wasm.I32False, dividend>,
-
-//   /** divisor */
-//   v extends WasmValue = Combine<divisor, Wasm.I32False>,
-
-//   /** quotient */
-//   q extends WasmValue = Wasm.I32False,
-
-//   _counter extends 1[] = [],
-
-//   _shiftedV extends WasmValue = ShiftRightOnceBinaryArbitrary<v, '0'>,
-//   _subtracted extends WasmValue = Ensure.I32<SubtractBinary<dr, _shiftedV>>,
-// > = Satisfies<{ quotient: string; remainder: string },
-
-//   _counter['length'] extends StopAt
-//   ? // game over
-//     // { dr: dr, v: v, q: q, _counter: _counter, _shiftedV: _shiftedV, _subtracted: _subtracted, _subractionIsNegative: _subractionIsNegative }
-//     { quotient: q; remainder: dr }
-
-//   : // recurse
-//     SignBit<_subtracted> extends '1'
-//     ?
-//       _DivideBinary32<
-//         dividend,
-//         divisor,
-//         dr,
-//         _shiftedV,
-//         ShiftLeftOnceBinaryArbitrary<q, '0'>,
-//         [1, ..._counter]
-//       >
-
-//     : 
-//       _DivideBinary32<
-//         dividend,
-//         divisor,
-//         ShiftRightOnceBinaryArbitrary<dr, '0'>,
-//         _shiftedV,
-//         ShiftLeftOnceBinaryArbitrary<q, '1'>,
-//         [1, ..._counter]
-//       >
-// >
-
-// export type DivideBinary32<
-//   dividend extends WasmValue,
-//   divisor extends WasmValue
-// > =
-//   divisor extends Wasm.I32False ? Wasm.I32False :
-//   _DivideBinary32<dividend, divisor>['quotient']
-
-// export type RemainderBinary32<
-//   dividend extends WasmValue,
-//   divisor extends WasmValue
-// > =
-//   divisor extends Wasm.I32False ? Wasm.I32False :
-//  Ensure.I32<_DivideBinary32<dividend, divisor>['remainder']>
+  >['quotient']
