87281936afbefea0969d9e667b1ccc30b70ccab0
teamchong <25894545+teamchong@users.noreply.github.com>
2024-03-20 12:25:14 -0400

    refactor: rewrite debug

--- packages/ts-type-math/tests/remainder-unsigned-binary.test.ts
+++ packages/ts-type-math/tests/remainder-unsigned-binary.test.ts
@@ -20,8 +20,8 @@ type i = 32
 type a = T[i]['a_binary']     // =>
 type b = T[i]['b_binary']     // =>
 type e = T[i]['rem_u_binary'] // =>
-type x = x1                   // =>
 type x1 = DivideUnsignedBinary32<a, b>['remainder']
+type x = x1                   // =>
 
 type Case00 = Expect<Equal<DivideUnsignedBinary32<T[ 0]['a_binary'], T[ 0]['b_binary']>['remainder'], T[ 0]['rem_u_binary']>>
 type Case01 = Expect<Equal<DivideUnsignedBinary32<T[ 1]['a_binary'], T[ 1]['b_binary']>['remainder'], T[ 1]['rem_u_binary']>>
