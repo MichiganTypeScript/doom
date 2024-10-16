53e873f2d69536f20fc7ddd26b828a449144cdfe
Dimitri Mitropoulos <dimitrimitropoulos@gmail.com>
2024-10-16 17:02:48 -0400

    fix multiple-return tests

--- packages/conformance-tests/from-c/multiple-returns.test.ts
+++ packages/conformance-tests/from-c/multiple-returns.test.ts
@@ -1,11 +1,25 @@
 import { expect, test } from 'vitest'
-import { getWasmMemory } from '../utils'
+import { getWasm, getWasmMemory } from '../utils'
 import type { entry } from './multiple-returns'
+import { Equal, Expect } from 'type-testing';
+import { ReadStringFromMemory } from '../../ts-type-math/memory-read-string';
 
 const name = 'multiple-returns';
 test(name, async () => {
-  const entry = getWasmMemory("from-c", name);
-  expect(await entry()).toStrictEqual(4);
+  const entry = await getWasm("from-c", name);
+  expect(entry()).toStrictEqual(1024);
+
+  const entryMemory = getWasmMemory("from-c", name);
+  expect(await entryMemory()).toStrictEqual("\u0004");
 });
 
-type result = entry<[], true>;
+type result = entry<[]>;
+//   ^?
+
+type t0 = Expect<Equal<result, 1024>>;
+
+type resultMemory = ReadStringFromMemory<entry<[], true>>;
+//   ^?
+
+type t1 = Expect<Equal<resultMemory, "\u0004">>;
+
