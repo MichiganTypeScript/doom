2dc56de57036fd9408e315c4fddab6492556b5b1
Dimitri Mitropoulos <dimitrimitropoulos@gmail.com>
2024-10-22 23:05:38 -0400

    some house cleaning now that this works

--- packages/playground/final-doom-pun-intended/data/mock-machine.ts
+++ packages/playground/final-doom-pun-intended/data/mock-machine.ts
@@ -4,13 +4,16 @@ import { executeInstruction } from "../../../wasm-to-typescript-types/program";
 import { ExpectedMemory, PointerBinary32 } from "./memory";
 import { evaluate } from "ts-type-math";
 import { MeetYourDoom } from "./process-frame";
+import { Expect, Equal } from "type-testing";
+import { ExpectedFrame } from "./expected-frame";
 
+/** This simulates the expected state when the program completes... err.. well.. one instruction before it completes */
 export type HolyShitIfThisActuallyWorksOMGOMGROFLWHATISWRONGWITHME = {
 	count: 9000;
 	results: null;
 	stack: [PointerBinary32];
 	instructions: [
-		{ kind: "EndFunction"; id: "$entry" }, // just one little function left
+		{ kind: "EndFunction"; id: "$entry" }, // just one little instruction left
 	];
 	activeLocals: {};
 	activeFuncId: "$entry";
@@ -24,7 +27,11 @@ export type HolyShitIfThisActuallyWorksOMGOMGROFLWHATISWRONGWITHME = {
 	executionContexts: [];
 	funcs: funcs;
 };
+/** finalize */
 export type HoldOntoYourMonadsAndBraceForImpact = executeInstruction<HolyShitIfThisActuallyWorksOMGOMGROFLWHATISWRONGWITHME, true, 9001>;
+
+/** pass the machine state to `MeetYourDoom` to get a frame */
 export type ActualFrame = MeetYourDoom<HoldOntoYourMonadsAndBraceForImpact>;
 
-
+/** if this test passes, it worked! */
+export type DidItActuallyWorkTho = Expect<Equal<ActualFrame, ExpectedFrame>>;
